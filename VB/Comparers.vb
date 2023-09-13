3bc43c13d23c13b7ce3c39e0e781437738248649
DevExpressExampleBot <fakeEmail>
2023-09-13 14:06:48 +0400

    autogenerate VB

--- VB/Comparers.vb
+++ VB/Comparers.vb
@@ -1,78 +1,68 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections
 Imports System.Collections.Generic
 Imports DevExpress.XtraScheduler
 
 Namespace SchedulerSortResources
-	Public MustInherit Class ResourceBaseComparer
-		Implements IComparer(Of Resource), IComparer
-		#Region "IComparer Members"
-		Private Function IComparer_Compare(ByVal x As Object, ByVal y As Object) As Integer Implements IComparer.Compare
-			Return CompareCore(x, y)
-		End Function
-		Public Function Compare(ByVal x As Resource, ByVal y As Resource) As Integer Implements IComparer(Of Resource).Compare
-			Return CompareCore(x, y)
-		End Function
-		#End Region
 
-		Protected Overridable Function CompareCore(ByVal x As Object, ByVal y As Object) As Integer
-			Dim xRes As Resource = CType(x, Resource)
-			Dim yRes As Resource = CType(y, Resource)
+    Public MustInherit Class ResourceBaseComparer
+        Inherits IComparer(Of Resource)
+        Implements IComparer
 
-			If xRes Is Nothing OrElse yRes Is Nothing Then
-				Return 0
-			End If
-			If Resource.Empty.Equals(xRes) OrElse Resource.Empty.Equals(yRes) Then
-				Return 0
-			End If
+#Region "IComparer Members"
+        Private Function Compare(ByVal x As Object, ByVal y As Object) As Integer
+            Return Me.CompareCore(x, y)
+        End Function
 
-			Return CompareResources(xRes, yRes)
-		End Function
+        Public Function Compare(ByVal x As Resource, ByVal y As Resource) As Integer
+            Return CompareCore(x, y)
+        End Function
 
-		Protected MustOverride Function CompareResources(ByVal xRes As Resource, ByVal yRes As Resource) As Integer
-	End Class
+#End Region
+        Protected Overridable Function CompareCore(ByVal x As Object, ByVal y As Object) As Integer
+            Dim xRes As Resource = CType(x, Resource)
+            Dim yRes As Resource = CType(y, Resource)
+            If xRes Is Nothing OrElse yRes Is Nothing Then Return 0
+            If Resource.Empty.Equals(xRes) OrElse Resource.Empty.Equals(yRes) Then Return 0
+            Return Me.CompareResources(xRes, yRes)
+        End Function
 
-	Public Class ResourceCaptionComparer
-		Inherits ResourceBaseComparer
-		Protected Overrides Function CompareResources(ByVal xRes As Resource, ByVal yRes As Resource) As Integer
-			Return String.Compare(xRes.Caption, yRes.Caption)
-		End Function
-	End Class
+        Protected MustOverride Function CompareResources(ByVal xRes As Resource, ByVal yRes As Resource) As Integer
+    End Class
 
-	Public Class ResourceCaptionReverseComparer
-		Inherits ResourceBaseComparer
-		Protected Overrides Function CompareResources(ByVal xRes As Resource, ByVal yRes As Resource) As Integer
-			Return String.Compare(yRes.Caption, xRes.Caption)
-		End Function
-	End Class
+    Public Class ResourceCaptionComparer
+        Inherits ResourceBaseComparer
 
-	Public Class ResourceNOfAppointmentsComparer
-		Inherits ResourceBaseComparer
-		Private schedulerStorage As SchedulerStorage
+        Protected Overrides Function CompareResources(ByVal xRes As Resource, ByVal yRes As Resource) As Integer
+            Return [String].Compare(xRes.Caption, yRes.Caption)
+        End Function
+    End Class
 
-		Public Sub New(ByVal schedulerStorage As SchedulerStorage)
-			Me.schedulerStorage = schedulerStorage
-		End Sub
+    Public Class ResourceCaptionReverseComparer
+        Inherits ResourceBaseComparer
 
-		Protected Overrides Function CompareResources(ByVal xRes As Resource, ByVal yRes As Resource) As Integer
-			' "Unassigned" resource
-			If Convert.ToInt32(yRes.Id) = -1 Then
-				Return 1
-			End If
+        Protected Overrides Function CompareResources(ByVal xRes As Resource, ByVal yRes As Resource) As Integer
+            Return [String].Compare(yRes.Caption, xRes.Caption)
+        End Function
+    End Class
 
-			' "Unassigned" resource
-			If Convert.ToInt32(xRes.Id) = -1 Then
-				Return -1
-			End If
+    Public Class ResourceNOfAppointmentsComparer
+        Inherits ResourceBaseComparer
 
-			Dim order As Integer = schedulerStorage.Appointments.Items.FindAll(Function(e) e.ResourceId.Equals(yRes.Id)).Count - schedulerStorage.Appointments.Items.FindAll(Function(e) e.ResourceId.Equals(xRes.Id)).Count
+        Private schedulerStorage As SchedulerStorage
 
-			If order = 0 Then
-				Return schedulerStorage.Resources.Items.IndexOf(xRes) - schedulerStorage.Resources.Items.IndexOf(yRes)
-			End If
+        Public Sub New(ByVal schedulerStorage As SchedulerStorage)
+            Me.schedulerStorage = schedulerStorage
+        End Sub
 
-			Return order
-		End Function
-	End Class
+        Protected Overrides Function CompareResources(ByVal xRes As Resource, ByVal yRes As Resource) As Integer
+            ' "Unassigned" resource
+            If Convert.ToInt32(yRes.Id) Is -1 Then Return 1
+            ' "Unassigned" resource
+            If Convert.ToInt32(xRes.Id) Is -1 Then Return -1
+            Dim order As Integer = schedulerStorage.Appointments.Items.FindAll(Function(e) e.ResourceId.Equals(yRes.Id)).Count - schedulerStorage.Appointments.Items.FindAll(Function(e) e.ResourceId.Equals(xRes.Id)).Count
+            If order Is 0 Then Return schedulerStorage.Resources.Items.IndexOf(xRes) - schedulerStorage.Resources.Items.IndexOf(yRes)
+            Return order
+        End Function
+    End Class
 End Namespace
